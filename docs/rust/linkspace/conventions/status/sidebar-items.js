38f50bc2328dd44b23d9581095fdfd633a278760
Azon <Antonsol919@gmail.com>
2023-10-04 11:23:10 +0200

    v0.5.0

--- docs/rust/linkspace/conventions/status/sidebar-items.js
+++ docs/rust/linkspace/conventions/status/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["is_status_reply","lk_status_overwatch","lk_status_request","lk_status_set","lk_status_watch"],"static":["STATUS_SPACE"],"struct":["LkStatus"]};
+window.SIDEBAR_ITEMS = {"fn":["lk_status_overwatch","lk_status_request","lk_status_set","lk_status_watch"],"static":["STATUS_SPACE"],"struct":["LkStatus"]};
--- docs/rust/linkspace/sidebar-items.js
+++ docs/rust/linkspace/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["PRIVATE","PUBLIC"],"mod":["abe","consts","conventions","key","misc","point","prelude","query","runtime","varctx"],"struct":["SigningKey"],"type":["LkError","LkResult"]};
+window.SIDEBAR_ITEMS = {"constant":["PRIVATE","PUBLIC"],"mod":["abe","consts","conventions","key","misc","point","prelude","query","runtime","varscope"],"struct":["SigningKey"],"type":["LkError","LkResult"]};
